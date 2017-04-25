eacbbd7d4a45d05c95576400a9324557891e1f56
Masataka SUMI <santarh@gmail.com>
2017-04-25 20:28:53 +0900

    Fix GetAccessToken

--- Assets/Mastodon/APIs/oauth/token.cs
+++ Assets/Mastodon/APIs/oauth/token.cs
@@ -11,13 +11,14 @@ namespace Mastodon
         public IObservable<string> GetAccessToken(string clientId, string clientSecret, string userName, string password)
         {
             return Post("/oauth/token", new Query
-            {
-                { "client_id", clientId },
-                { "client_secret", clientSecret },
-                { "grant_type", "password" },
-                { "username", userName },
-                { "password", password },
-            });
+                {
+                    {"client_id", clientId},
+                    {"client_secret", clientSecret},
+                    {"grant_type", "password"},
+                    {"username", userName},
+                    {"password", password},
+                }).Select(JsonMapper.ToObject)
+                .Select(x => x.GetStringOrElse("access_token"));
         }
     }
 }
